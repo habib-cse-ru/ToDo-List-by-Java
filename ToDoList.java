a2d375e74f5bd43a4f27660f117383474ff75975
Habibur Rahman <habib.cse.ru@gmail.com>
2023-09-06 00:53:25 +0600

    maek as Done is section is complited

--- ToDoList.java
+++ ToDoList.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class ToDoList {
 
     Scanner sc = new Scanner(System.in);
-
     /**
      * this will add a task to the ToDo list arryList
      * @param todoList this is the ArryList which contains the ToDo List
@@ -15,4 +14,15 @@ public class ToDoList {
         StringBuilder work = new StringBuilder(workString);
         todoList.add(work);
     }
+    /**
+     * this will mark a work is done . to do so it will take a input the task index
+     * @param todoList this is the ToDo list container ArrayList
+     */
+    public void markDone(ArrayList<StringBuilder> todoList) {
+        System.out.println("Enter the idex of work");
+        int position = sc.nextInt();
+        todoList.get(position - 1).insert(1, "X");
+
+    }
+
 }
--- ToDoListRunner.java
+++ ToDoListRunner.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ToDoListRunner {
+    static ArrayList<StringBuilder> todoList = new ArrayList<StringBuilder>();
 
     public static void main(String[] args) {
         System.out.println("Welcome to To-Do List Application");
@@ -14,8 +16,8 @@ public class ToDoListRunner {
             System.out.println("5.Exit");
             System.out.println("Enter the index to move foreward :");
             int response = sc.nextInt();
-            if (response == 1) {
-            } else if (response == 2) {
+            if (response == 1) { listOne.addTask(todoList);
+            } else if (response == 2) { listOne.markDone(todoList);
             } else if (response == 3) {
             } else if (response == 4) {
             } else if (response == 5) {

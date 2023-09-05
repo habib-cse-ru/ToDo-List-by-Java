fa9d515f4e06823a6fc3a9e7e103a9fe91f3b136
Habibur Rahman <habib.cse.ru@gmail.com>
2023-09-06 00:41:48 +0600

    ToDo List runner tamplet is done

--- ToDoListRunner.java
+++ ToDoListRunner.java
@@ -1,6 +1,29 @@
-public class ToDoListRunner{
+import java.util.Scanner;
+
+public class ToDoListRunner {
 
     public static void main(String[] args) {
-        
+        System.out.println("Welcome to To-Do List Application");
+        while (true) {
+            ToDoList listOne = new ToDoList();
+            Scanner sc = new Scanner(System.in);
+            System.out.println("1. Add Task");
+            System.out.println("2.Mark Task As Complited .");
+            System.out.println("3.Task List");
+            System.out.println("4.Delete Conplited Task");
+            System.out.println("5.Exit");
+            System.out.println("Enter the index to move foreward :");
+            int response = sc.nextInt();
+            if (response == 1) {
+            } else if (response == 2) {
+            } else if (response == 3) {
+            } else if (response == 4) {
+            } else if (response == 5) {
+                break;
+            } else {
+            }
+
+        }
+
     }
 }

32eb5054244a6963677f82e8546d6b9889e2ccee
Habibur Rahman <habib.cse.ru@gmail.com>
2023-09-06 00:45:21 +0600

    add task to the ToDo list is done

--- ToDoList.java
+++ ToDoList.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ToDoList {
-    
+
+    Scanner sc = new Scanner(System.in);
+
+    /**
+     * this will add a task to the ToDo list arryList
+     * @param todoList this is the ArryList which contains the ToDo List
+     */
+    public void addTask(ArrayList<StringBuilder> todoList) {
+        System.out.println("Enter Your Task:");
+        String workString = "[]" + sc.nextLine();
+        StringBuilder work = new StringBuilder(workString);
+        todoList.add(work);
+    }
 }
